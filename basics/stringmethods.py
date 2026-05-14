21ec9d1a0dbd2fecf4bea55a9ca494de67deb2d7
c shivani <workwithcshivani@gmail.com>
2026-05-14 21:07:27 +0530

    string methods

--- basics/stringmethods.py
+++ basics/stringmethods.py
@@ -1,3 +1,7 @@
-text = "hi";
-
-print(text.upper());
+text = "my name is shivani"
+########## CASE METHODS  #########
+print(text.upper())
+print(text.lower())
+print(text.capitalize())
+print(text.title())
+print(text.swapcase())
